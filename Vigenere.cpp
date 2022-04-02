1027bdae68e9635e0aa3b793fedf273fb2a52286
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-04-02 13:30:11 -0700

    Added Vigenere Skeleton Code

---
+++ Vigenere.cpp
@@ -0,0 +1,38 @@
+#include "Vigenere.h"
+
+
+/**
+ * Sets the key to use
+ * @param key - the key to use
+ * @return - True if the key is valid and False otherwise
+ */
+bool Vigenere::setKey(const string& key)
+{
+
+	return false;
+}
+
+
+
+
+/**
+ * Encrypts a plaintext string
+ * @param plaintext - the plaintext string
+ * @return - the encrypted ciphertext string
+ */
+string Vigenere::encrypt(const string& plaintext)
+{
+
+	return "";
+}
+
+/**
+ * Decrypts a string of ciphertext
+ * @param cipherText - the ciphertext
+ * @return - the plaintext
+ */
+string Vigenere::decrypt(const string& cipherText)
+{
+
+	return "";
+}
