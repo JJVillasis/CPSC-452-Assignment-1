0fad5e6f7b3e8df0913a898beed3939cc02ce2eb
Joshua Villasis <JJVillasis@csu.fullerton.edu>
2022-04-02 14:16:24 -0700

    Added sections: Parse Command Arguments, Cipher Interface

--- cipher.cpp
+++ cipher.cpp
@@ -1,37 +1,79 @@
+#include <iostream>
 #include <string>
 #include "CipherInterface.h"
 #include "Playfair.h"
+#include "Railfence.h"
+#include "Vigenere.h"
+#include "Caesar.h"
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	/* REPLACE THIS PART WITH YOUR CODE 
-	 * THE CODE BELOW IS A SAMPLE TO 
-	 * ILLUSTRATE INSTANTIATION OF CLASSES
-	 * THAT USE THE SAME INTERFACE.
-	 */	
-		
+	// Parsing Command Line Arguments //
 
-	/* Create an instance of the Playfair cipher */	
-	CipherInterface* cipher = new Playfair();
-	
-	/* Error checks */
-	if(!cipher)
+	string cipherName = argv[1];
+	string key = argv[2];
+	string encDec = argv[3];
+	string inputFile = argv[4];
+	string outputFile = argv[5];
+
+
+	// Handling Cipher Interface //
+
+	CipherInterface* cipher = NULL;
+
+	//If the Cipher is Playfair
+	if(cipherName == "PLF")
 	{
-		fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",	
-		__FILE__, __FUNCTION__, __LINE__);
-		exit(-1);
+		cipher = new Playfair();
+		cipher->setKey(key);
 	}
-	
-	/* Set the encryption key */
-	cipher->setKey("security");
-	
-	/* Perform encryption */
-	string cipherText = cipher->encrypt("hello world");
-	
-	/* Perform decryption */
-	cipher->decrypt(cipherText);	
-	
+	//If the cipher is Railfence
+	else if(cipherName == "RFC")
+	{
+		cipher = new Railfence();
+		cipher->setKey(key);
+	}
+	//If the cipher is Vigenere
+	else if(cipherName == "VIG")
+	{
+		cipher = new Vigenere();
+		cipher->setKey(key);
+	}
+	//If the cipher is Caesar
+	else if(cipherName == "CES")
+	{
+		cipher = new Caesar();
+		cipher->setKey(key);
+	}
+	//If the cipher is unknown/unimplemented
+	else
+	{
+		cout << "The Cipher \"" << cipherName << "\" is unknown.\n";
+		exit(1);
+	}
+
+
+	// Performing the Encryption/Decryption //
+
+	//Encryption process
+	if(encDec == "ENC")
+	{
+
+	}
+	//Decryption process
+	else if(encDec == "DEC")
+	{
+
+	}
+	//Unknown process
+	else
+	{
+		cout << "Unknown Process: \"" << encDec << "\"\n";
+		exit(1);
+	}
+
+
 	return 0;
 }
