11a0220ffa51a7714359dd7fd52ce5822a67525c
Evan Wu <earth2pig@yahoo.com>
2022-04-09 17:07:54 -0700

    Update Railfence.cpp

--- Railfence.cpp
+++ Railfence.cpp
@@ -6,10 +6,12 @@
  * @param key - the key to use
  * @return - True if the key is valid and False otherwise
  */
-bool Railfence::setKey(const string& key)
+bool Railfence::setKey(const int& key)
 {
-
-	return false;
+	if (key <= 0)
+		return false;
+	else
+		return true;
 }
 
 
@@ -22,9 +24,35 @@ bool Railfence::setKey(const string& key)
  */
 string Railfence::encrypt(const string& plaintext)
 {
+	// creating a matrix to hold the plaintext
+	char railMatrix[cipherKey][plaintext.length()];
 
+	// filling railMatrix with blank spaces
+	for (int i=0; i < cipherKey; i++)
+		for (int j=0; j < plaintext.length(); j++)
+			railMatrix[i][j] = '\n';
 
-	return "";
+	for (int i = 0; i < plaintext.length(); i++){
+		// fill with the correct letter
+		railMatrix[row][col++] = plaintext[i];
+
+		// if we just filled the top or bottom row reverse direction
+		if(row == 0 || row == cipherKey-1)
+			direction = direction * (-1);
+
+		// move onto next row
+		row = row + direction;
+	}
+	row = col = 0;
+	direction = -1;
+
+	string result;
+	for (int i=0; i < cipherKey; i++)
+		for (int j=0; j < plaintext.length(); j++)
+			if (railMatrix[i][j] != '\n')
+				result.push_back(railMatrix[i][j]);
+
+	return result;
 }
 
 /**
@@ -34,6 +62,46 @@ string Railfence::encrypt(const string& plaintext)
  */
 string Railfence::decrypt(const string& cipherText)
 {
-	return "";
+	// creating a matrix to hold the cipherText
+	char railMatrix[cipherKey][cipherText.length()];
+
+	// filling railMatrix with blank spaces
+	for (int i=0; i < cipherKey; i++)
+		for (int j=0; j < cipherText.length(); j++)
+			railMatrix[i][j] = '\n';
+	
+	for (int i=0; i < cipherText.length(); i++){
+		// mark spaces that should hold text with a placeholder
+		railMatrix[row][col] = '*';
+
+		// if we just filled the top or bottom row reverse direction
+		if(row == 0 || row == cipherKey-1)
+			direction = direction * (-1);
+
+		//move onto next row
+		row = row + direction;
+	}
+
+	// fill in the positions indicated with * with letters
+	for(int i=0; i < cipherKey; i++)
+		for(int j=0; j < cipherText.length(); j++)
+			if(railMatrix[i][j] == '*')
+				railMatrix [i][j] = cipherText[index++];
+
+	row = col = 0;
+	direction = -1;	
+
+	string result;
+
+	for (int i = 0; i < cipherText.length(); i++){
+		result.push_back(railMatrix[row][col++]);
+ 
+        if(row == 0 || row == cipherKey-1)
+            direction = direction * (-1);
+ 
+        row = row + direction;
+	}
+
+	return result;
 
 }
